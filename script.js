091a575f3162511f1e3d7c4a459315ebf7b8fe3f
Jessica <jessicaaparecidacandido715@gmail.com>
2026-04-28 10:02:44 -0300

    primeira conexão do projeto

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.getElementById("studentForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
@@ -12,4 +13,20 @@ document.getElementById("studentForm").addEventListener("submit", function (e) {
   ).innerText = `Cadastro realizado com sucesso, ${name}!`;
 
   this.reset();
+=======
+document.getElementById("studentForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  localStorage.setItem("studentName", name);
+  localStorage.setItem("studentEmail", email);
+
+  document.getElementById(
+    "successMessage"
+  ).innerText = `Cadastro realizado com sucesso, ${name}!`;
+
+  this.reset();
+>>>>>>> 1cda2b5 (primeira conexão do projeto)
 });
